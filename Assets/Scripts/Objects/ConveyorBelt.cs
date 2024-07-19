daf7910eed57cd46b48c5d315889173b440597ab
Daniel Pascual López <uo269728@uniovi.es>
2024-07-20 01:05:49 +0200

    Cambio de nombre

--- Assets/Scripts/Objects/ConveyorBelt.cs
+++ Assets/Scripts/Objects/ConveyorBelt.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Belt : PlacedObject
+public class ConveyorBelt : PlacedObject
 {
     // Start is called before the first frame update
     void Start()
